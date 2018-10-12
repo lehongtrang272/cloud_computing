ab40c8660d3f7862ae1c8c3611bdd90396ec470f
lehongtrang272 <lehongtrang1992@gmail.com>
2018-10-12 13:47:47 +0200

    minor fix

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
