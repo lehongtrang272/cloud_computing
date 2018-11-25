e5a35edba8c5e2621c7f82a8123a80286385737f
lehongtrang272 <lehongtrang1992@gmail.com>
2018-11-25 21:49:42 +0100

    fixing certificate

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const SocketIOFile = require('socket.io-file');
 var fs = require('fs');
 
 const option = {
-	key: fs.readFileSync('tls/Mykey.key'),
-	cert:fs.readFileSync('tls/MyCertificate.crt')
+	key: fs.readFileSync('tls/server.key'),
+	cert:fs.readFileSync('tls/server_cert.crt')
 }
 
 var port = process.env.PORT || 443;
