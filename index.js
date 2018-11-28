872e031c35d59dbbac97e8cee994620e4cc851d3
felixxs <schoch.felix@web.de>
2018-11-28 14:52:32 +0100

    imageBase64 parser

--- index.js
+++ index.js
@@ -257,6 +257,23 @@ console.log('done');
    
 });
 
+/**
+ * Parse a base 64 image and return the extension and buffer
+ * @param  {String} imageString The image data as base65 string
+ * @return {Object}             { type: String, data: Buffer }
+ */
+function parseBase64Image(imageString) {
+	var matches = imageString.match(/^data:image\/([A-Za-z-+/]+);base64,(.+)$/);
+	var resource = {};
+  
+	if (matches.length !== 3) {
+	  return null;
+	}
+  
+	resource.type = matches[1] === 'jpeg' ? 'jpg' : matches[1];
+	resource.data = new Buffer(matches[2], 'base64');
+	return resource;
+  }
 
 function requireHTTPS(req,res,next){
 	if( req.headers && req.headers.$wssp === "80"){
